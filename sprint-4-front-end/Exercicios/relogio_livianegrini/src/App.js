e763fceb661c5093f374490b90755640790506c9
livianegrini <liviasnegrini2004@gmail.com>
2021-10-20 14:37:02 -0300

    Relogio funcionando

--- sprint-4-front-end/Exercicios/relogio_livianegrini/src/App.js
+++ sprint-4-front-end/Exercicios/relogio_livianegrini/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function DataFormatada(props){
-  return <h2>Horário Brasil: {props.date}</h2>
+  return <h2>Horário Brasil: {props.date.toLocaleTimeString()}</h2>
 }
 
 class Clock extends React.Component{
@@ -18,13 +18,46 @@ class Clock extends React.Component{
   // Ciclo de vida que ocorre quando Clock é inserido na DOM
   // Nascimento
   componentDidMount(){
+    this.timerID = setInterval( () => {
+      this.thick()
+    }, 1000 )
 
+   // Exibe no console o ID de cada relógio
+   console.log('Eu sou o relógio ' + this.timerID);
   }
+  
 
   // Ciclo de vida que ocorre quando Clock é removido na DOM
   // Morte
+  // Quando isso acontece, a função clearInterval() limpa o relógio criado pela setInterval
   componentWillUnmount(){
+    clearInterval(this.timerID)
+  }
 
+  // Simula as batidas do relógio
+  thick(){
+    this.setState({
+      date : new Date()
+    })
+  }
+
+  PausarRelogio(){
+
+    clearInterval(this.timerID);
+
+    // Exibe no console o ID de cada relógio
+    console.log('Relógio ' + this.timerID + ' pausado!');
+  }
+
+  RetomarRelogio(){
+
+    this.timerID = setInterval( () => {
+      this.thick()
+    }, 1000 )
+
+    // Exibe no console o ID de cada relógio
+    console.log('Relógio retomado!');
+    console.log('Agora eu sou o relógio ' + this.timerID);
   }
 
   render(){
@@ -32,10 +65,13 @@ class Clock extends React.Component{
       <div> 
         <h1>Relógio</h1>
         <DataFormatada date={this.state.date}/>
+        <div className="Botoes">
+         <button className="BotaoPausar" onClick={() => {this.PausarRelogio()}}>Pausar</button>
+         <button className="BotaoRetomar" onClick={() => {this.RetomarRelogio()}}>Retomar</button>
+        </div>
       </div>
     )
   }
-
 }
 
 function App() {
